586c840dd9b907cd6c2a1a52beebe681cae53bf3
lussivasko <ludmillka19@gmail.com>
2019-04-03 15:48:53 +0300

    toTop

--- app/js/common.js
+++ app/js/common.js
@@ -4,3 +4,20 @@
     // $('body').fadeOut();
 
 });
+
+
+
+$(document).ready(function(){
+
+	$(window).scroll(function() {
+		if($(this).scrollTop() != 0) {
+			$('.to_top').fadeIn();
+		} else {
+			$('.to_top').fadeOut();
+		}
+	});
+
+	$('.to_top').click(function() {
+		$('body, html').animate({scrollTop:0},800);
+  });
+})
